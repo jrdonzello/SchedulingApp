f934c4e3c6e3261eafda772b48a58f6db77a66eb
aclanan <aclanan@gmail.com>
2019-04-23 15:51:50 -0600

    PLz work

---
+++ app/src/main/java/cs4330/cs/utep/scheduleapp/DisplaySchedule.java
@@ -0,0 +1,107 @@
+package cs4330.cs.utep.scheduleapp;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class DisplaySchedule extends AsyncTask<String,Void,String> {
+    protected String name = "aclanan";
+    protected String start;
+    protected String end;
+    protected String track;
+
+    public DisplaySchedule(){
+
+    }
+
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+//        String jsonResponse = null;
+//        String json_data = params[0];
+//        HttpURLConnection httpURLConnection = null;
+//        BufferedReader bufferedReader = null;
+        Log.d("ANDREW","Downloading...");
+        try {
+            URL url = new URL("http://helper.at.utep.edu/scheduling_app/EmployeeSchedule.php");
+
+            JSONObject postDataParams = new JSONObject();
+            postDataParams.put("name", name);
+            Log.d("ANDREW", postDataParams.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+            writer.flush();
+            writer.close();
+            os.close();
+
+            int responseCode = conn.getResponseCode();
+            if(responseCode == HttpURLConnection.HTTP_OK){
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuffer sb = new StringBuffer();
+                String line = "";
+
+                while( (line = in.readLine()) != null){
+                    sb.append(line);
+                }
+                in.close();
+                return sb.toString();
+            }else{
+                return "False: " + responseCode;
+            }
+
+        }catch(Exception e){
+            return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    public String getPostDataString(JSONObject params)throws Exception{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+        while(itr.hasNext()){
+            String key = itr.next();
+            Object value = params.get(key);
+            if(first){
+                first = false;
+            }else{
+                result.append("&");
+            }
+            result.append(URLEncoder.encode(key,"UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(),"UTF-8"));
+        }
+        return result.toString();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        Log.d("ANDREW", result);
+    }
+}
+
